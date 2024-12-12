67532feccd28311501f5794f3a15bd91eb03b4be
Kahn <958984289@qq.com>
2024-12-12 15:14:58 +0800

    更新新功能

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: ({name}) => {
+          if (/\.(css|less|sass|scss)$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  }
 })
